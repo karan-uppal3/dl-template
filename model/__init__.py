74b6047be87d1432fcb800bc075ecdd7922261a3
karan-uppal3 <57282321+karan-uppal3@users.noreply.github.com>
2021-02-07 22:54:14 +0530

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,5 @@
 from model.enet import ENet
+from model.deeplab import Deeplab
 import torch.optim as optim
 
 
@@ -6,6 +7,8 @@ def CreateModel(args):
 
     if args.model == 'enet':
         model = ENet(num_classes=args.num_classes)
+    elif args.model == 'deeplab':
+        model = Deeplab(num_classes=args.num_classes)
     else:
         raise ValueError('The model must be enet')
 
