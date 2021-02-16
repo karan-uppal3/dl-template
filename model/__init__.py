e7116c9b0a8f63699adea8c3c1ef6d5e5d3032a1
saurabhmishra608 <saurabhmishra608@gmail.com>
2021-02-16 19:26:54 +0530

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,7 @@
 from model.enet import ENet
 from model.deeplab import Deeplab
 from model.FRRNet import FRRNet
+from model.fcn import FCN8s
 import torch.optim as optim
 
 
@@ -19,7 +20,10 @@ def CreateModel(args):
         model = FRRNet(out_channels=args.num_classes).cuda()
         optimizer = optim.Adam(
             model.parameters(), lr=args.learning_rate)
-
+    elif args.model == 'fcn8s':
+        model = FCN8s(n_class=args.num_classes).cuda()
+        optimizer = optim.SGD(
+            model.parameters(), lr=args.learning_rate)
     else:
         raise ValueError('The model must be enet/deeplab/frrnet')
 
